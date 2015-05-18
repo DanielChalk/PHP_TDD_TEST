3c9f875b7b01764d27848040330418d43d17cbb7
Daniel Chalk <daniel.chalk@wtg.co.uk>
2015-05-18 15:47:32 +0100

    Simplified test a little because i'd prefer a simple method was the focus.

--- src/Template.php
+++ src/Template.php
@@ -1,62 +1,18 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: Daniel Chalk <daniel@containsflashingimages.com>
  * Date: 06/05/2015
  * Time: 11:06
  */
-
 class Template
 {
-    /**
-     * path to a template
-     * @var string
-     */
-    protected $template;
-
-    /**
-     * array of keys and values to be used within the template
-     * @var mixed[]
-     */
-    protected $variables;
-
-    /**
-     * @param $template
-     * @param $variables
-     */
-    public function __construct($template, $variables)
-    {
-        $this->template = $template;
-        $this->variables = $variables;
-    }
-
-    /**
-     * @param $template
-     * @param $variables
-     * @return string
-     */
-    public static function make($template, $variables)
-    {
-        $template = new self($template, $variables);
-        return $template->render();
-    }
-
-    public function render()
-    {
-        $template = __DIR__ . '/' . $this->template;
-
-        if(!file_exists($template)) {
-            throw new Exception('Template not found');
-
-        }
-
-        ob_start();
-
-        extract($this->variables);
-        require($template);
-
-        return ob_get_clean();
-
-
-    }
+  /**
+   * @return string
+   */
+  public static function render($template, $values)
+  {
+    throw new Exception('Not implemented');
+  }
 }
--- tests/TemplateTest.php
+++ tests/TemplateTest.php
@@ -8,21 +8,21 @@
  */
 class TemplateTest extends PHPUnit_Framework_TestCase
 {
-    public function testTemplateIncludesUsername()
-    {
-        $template ='templates/user.php';
-        $variables = ['username' => 'Demo user'];
-        $view = Template::make($template, $variables);
-        $this->assertContains('Demo user', $view);
-    }
+  public function testTemplateIncludesUsername()
+  {
+    $template  = 'user';
+    $variables = ['username' => 'Demo user'];
+    $view      = Template::render($template, $variables);
+    $this->assertContains('Demo user', $view);
+  }
 
-    /**
-     * @expectedException   Exception
-     * @expectedExceptionMessage Template not found
-     */
-    public function testTemplateThrowsWhenNoFileExists()
-    {
-        $template = 'templates/error.php';
-        Template::make($template, array());
-    }
+  /**
+   * @expectedException   Exception
+   * @expectedExceptionMessage Template not found
+   */
+  public function testTemplateThrowsWhenNoFileExists()
+  {
+    $template = 'does_not_exists';
+    Template::render($template, array());
+  }
 }
